f998c4ff37035c710d1d7d8c1e58e0c7332ad899
joeottofaro <joseph.ottofaro@gmail.com>
2026-02-04 14:56:18 +0000

    paddle class finished

--- paddle.py
+++ paddle.py
@@ -7,21 +7,14 @@ HEIGHT = 5
 class Paddle(Turtle):
     def __init__(self, starting_position):
         super().__init__()
-        xy_pos = starting_position
-        self.paddle = self.create_paddle(xy_pos)
-
-    def create_paddle(self, position):
-        new_paddle = Turtle("square")
-        new_paddle.color("white")
-        new_paddle.penup()
-        new_paddle.shapesize(stretch_wid=HEIGHT, stretch_len=WIDTH)
-        new_paddle.goto(position)
-        return new_paddle
+        self.shape("square")
+        self.color("white")
+        self.penup()
+        self.shapesize(stretch_wid=HEIGHT, stretch_len=WIDTH)
+        self.goto(starting_position)
 
     def go_up(self):
-        new_y = self.paddle.ycor() + 20
-        self.paddle.goto(self.paddle.xcor(), new_y)
+        self.goto(self.xcor(), self.ycor() + 20)
 
     def go_down(self):
-        new_y = self.paddle.ycor() - 20
-        self.paddle.goto(self.paddle.xcor(), new_y)
+        self.goto(self.xcor(), self.ycor() - 20)
