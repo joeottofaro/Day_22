551bd66eeb3041213557b0e39471259ad45afad3
joeottofaro <joseph.ottofaro@gmail.com>
2026-02-04 17:56:48 +0000

    ball out of bounds done

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ player_1 = Paddle((350, 0))
 player_2 = Paddle((-350, 0))
 ball = Ball()
 
-
 screen.onkeypress(player_1.go_up, "Up")
 screen.onkeypress(player_1.go_down, "Down")
 screen.onkeypress(player_2.go_up, "w")
@@ -27,10 +26,16 @@ while game_is_on:
     ball.move()
     # Detect collision with wall
     if ball.ycor() > 280 or ball.ycor() < -280:
-        print("Pong")
         ball.bounce_y()
     # Detect collision with paddle
     if ball.distance(player_1) < 50 and ball.xcor() > 320 or ball.distance(player_2) < 50 and ball.xcor() < -320:
         ball.bounce_x()
+    # Detect if player 1 missed the ball
+    if ball.xcor() > 380:
+        ball.reset_ball()
+
+    # Detect if player 2 missed the ball
+    if ball.xcor() < -380:
+        ball.reset_ball()
 
 screen.exitonclick()
